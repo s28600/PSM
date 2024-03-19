807f724f904944e1fec9eec69f4e9a5243ffc850
Illya Moskalenko <illya.moskalenko@gmail.com>
2024-03-19 19:02:14 +0100

    Moved standard Euler to its own method

--- Idea/src/C02/Main.java
+++ Idea/src/C02/Main.java
@@ -8,6 +8,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Scanner;
 
@@ -42,20 +43,50 @@ public class Main {
             System.out.println("Data accepted.");
         } else System.out.println("Standard configuration will be used.");
 
+        List<List<Double>> standardEuler = standardEuler(0, 0, 7, 7, 0, 0.05, 0, -9.81, 1, 0.47);
+        List<Double> time = standardEuler.get(0);
+        List<Double> xValues = standardEuler.get(1);
+        List<Double> yValues = standardEuler.get(2);
+
+        Path outputFilePath = Paths.get("data.csv");
+        Files.deleteIfExists(outputFilePath);
+        Files.createFile(outputFilePath);
+        BufferedWriter writer = new BufferedWriter(new FileWriter("data.csv", true));
+
+        writer.append("Standard Euler,,\nt,x,y\n");
+        for (int i = 0; i < xValues.size(); i++) {
+            writer.append(time.get(i) + "," + xValues.get(i) + "," + yValues.get(i) + "\n");
+        }
+        writer.close();
+
+        System.out.print("Trajectory data was written to file, path: " + outputFilePath.toAbsolutePath());
+    }
+
+    public static List<List<Double>> standardEuler(
+            double Sx,
+            double Sy,
+            double Vx,
+            double Vy,
+            double t,
+            double dt,
+            double gx,
+            double gy,
+            double m,
+            double Cx
+    ){
         double ax = (m*gx-Cx*Vx)/m; //x-axis acceleration
         double ay = (m*gy-Cx*Vy)/m; //y-axis acceleration
         double dSx = Vx*dt; //Change of speed in x-axis
         double dSy = Vy*dt; //Change of speed in y-axis
         double dVx = ax*dt; //Change of x-axis acceleration
         double dVy = ay*dt; //Change of y-axis acceleration
+
         List<Double> xValues = new ArrayList<>();
         List<Double> yValues = new ArrayList<>();
         List<Double> time = new ArrayList<>();
         xValues.add(Sx);
         yValues.add(Sy);
         time.add(t);
-
-        //Standard Euler
 
         while (Sy >= 0) {
             t += dt;
@@ -75,17 +106,10 @@ public class Main {
             yValues.add(Sy);
         }
 
-        Path outputFilePath = Paths.get("data.csv");
-        Files.deleteIfExists(outputFilePath);
-        Files.createFile(outputFilePath);
-        BufferedWriter writer = new BufferedWriter(new FileWriter("data.csv", true));
-
-        writer.append("Standard Euler,,\nt,x,y\n");
-        for (int i = 0; i < xValues.size(); i++) {
-            writer.append(time.get(i) + "," + xValues.get(i) + "," + yValues.get(i) + "\n");
-        }
-        writer.close();
-        
-        System.out.print("Trajectory data was written to file, path: " + outputFilePath.toAbsolutePath());
+        List<List<Double>> out = new ArrayList<>();
+        out.add(time);
+        out.add(xValues);
+        out.add(yValues);
+        return out;
     }
 }
