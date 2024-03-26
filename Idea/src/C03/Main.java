c2d1d000beb284993b31a06b6bd44eeedc12a997
s28600 <s28600@pjwstk.edu.pl>
2024-03-26 21:34:21 +0100

    Implemented euler and midpoint, Ec output for testing

--- Idea/src/C03/Main.java
+++ Idea/src/C03/Main.java
@@ -29,8 +29,17 @@ public class Main {
         } else System.out.println("Standard configuration will be used.");
 
         List<String> euler = euler(a, w, t, dt, l, g);
+        List<String> midpoint = midpoint(a, w, t, dt, l, g);
 
-        writeDataToFile(euler);
+        //Adding both to compare graphs
+        List<String> compare = new ArrayList<>();
+        String str;
+        for (int i = 0; i < euler.size(); i++) {
+            str = euler.get(i)+",,"+midpoint.get(i);
+            compare.add(str);
+        }
+
+        writeDataToFile(compare);
     }
 
     public static List<String> euler(
@@ -42,15 +51,47 @@ public class Main {
             Double g
     ){
         List<String> data = new ArrayList<>();
-        data.add(t+","+a+","+w+"\n");
+        data.add(t+","+a+","+w+","+Ec(l,w,a));
 
-        while(t<20) {
+        while(t<10) {
             t += dt;
             double e = g/l*Math.sin(a); //rotational acceleration
             a += w*dt;
             w += e*dt;
 
-            data.add(t+","+a+","+w+"\n");
+            data.add(t+","+a+","+w+","+Ec(l,w,a));
+        }
+
+        return data;
+    }
+
+    public static List<String> midpoint(
+            Double a,
+            Double w,
+            Double t,
+            Double dt,
+            Double l,
+            Double g
+    ){
+        List<String> data = new ArrayList<>();
+        data.add(t+","+a+","+w+","+Ec(l,w,a));
+
+        //First cycle
+        t += dt;
+        double e = g/l*Math.sin(a);
+        w += e*dt/2;
+        a += w*dt;
+
+        data.add(t+","+a+","+w+","+Ec(l,w,a));
+
+        //Continuation
+        while(t<10) {
+            t += dt;
+            e = g/l*Math.sin(a);
+            a += w*dt;
+            w += e*dt;
+
+            data.add(t+","+a+","+w+","+Ec(l,w,a));
         }
 
         return data;
@@ -63,10 +104,21 @@ public class Main {
         BufferedWriter writer = new BufferedWriter(new FileWriter("data.csv", true));
 
         for (String str : data) {
-            writer.append(str);
+            writer.append(str).append("\n");
         }
 
         writer.close();
         System.out.print("Trajectory data was written to file, path: " + outputFilePath.toAbsolutePath());
+    }
+
+    public static double Ec(double l, double w, double a){
+        double y = -l*Math.sin(Math.PI/2-a);
+        double v = w*l;
+        double h = y+l;
+        double Ep = 9.81*h;
+        double Ek = Math.pow(v, 2)/2;
+        double Ec = Ep+Ek;
+
+        return Ec;
     }
 }
