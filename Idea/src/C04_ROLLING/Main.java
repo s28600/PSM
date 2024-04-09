4690b13978d4d75edc1219d49f4a1d0bc99b92cd
s28600 <s28600@pjwstk.edu.pl>
2024-04-09 21:30:36 +0200

    Maybe working midpoint for position

--- Idea/src/C04_ROLLING/Main.java
+++ Idea/src/C04_ROLLING/Main.java
@@ -33,27 +33,47 @@ public class Main {
 
     public static List<String> calculateTrajectory(SphericalObject object){
         List<String> data = new ArrayList<>();
-        double t = 0, Sx = 0, Sy = object.r, Vx = 0;
-        data.add(t+sep+Sx+sep+Sy+sep+Vx);
+        double t = 0, Sx = 0, Sy = object.r, Vx = 0,
+                a = g*Math.sin(alpha)/(1+object.I/(m*Math.pow(r, 2))),
+                Sx_o = Sx*Math.cos(-alpha)-Sy*Math.sin(-alpha),
+                Sy_o = Sx*Math.sin(-alpha)+Sy*Math.cos(-alpha)+h;
+        data.add(t+sep+Sx+sep+Sy+sep+Vx+sep+Sx_o+sep+Sy_o);
 
         double[] k;
         while (Sx < d){
             t += dt;
 
-            k = derivatives(Vx, object);
+            k = derivatives(Sx, Vx, a);
             Sx += k[0] * dt;
             Vx += k[1] * dt;
 
-            data.add(t+sep+Sx+sep+Sy+sep+Vx);
+            Sx_o = Sx*Math.cos(-alpha)-Sy*Math.sin(-alpha);
+            Sy_o = Sx*Math.sin(-alpha)+Sy*Math.cos(-alpha)+h;
+
+            data.add(t+sep+Sx+sep+Sy+sep+Vx+sep+Sx_o+sep+Sy_o);
         }
 
         return data;
     }
 
-    public static double[] derivatives(double Vx, SphericalObject object){
+    public static double[] derivatives(double Sx, double Vx, double a){
         double[] out = new double[2];
         out[0] = Vx;
-        out[1] = g*Math.sin(alpha)/(1+object.I/(m*Math.pow(r, 2)));
+        out[1] = a;
+        return out;
+    }
+
+    public static double[] solveMidpoint(double Sx, double Vx, double a){
+        double[] out = new double[2];
+
+        double[] k = derivatives(Sx, Vx, a);
+        Sx += k[0] * dt/2;
+        Vx += k[1] * dt/2;
+
+        k = derivatives(Sx, Vx, a);
+        out[0] = Sx + k[0] * dt;
+        out[1] = Vx + k[1] * dt;
+
         return out;
     }
 }
