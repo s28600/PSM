50cbfaf42014a7d1fe2f34f8c94e592c716b67e9
s28600 <s28600@pjwstk.edu.pl>
2024-04-23 21:27:02 +0200

    Euler points position calculation working

--- Idea/src/C06_StringMotion/Util.java
+++ Idea/src/C06_StringMotion/Util.java
@@ -17,23 +17,42 @@ public class Util {
         double[] pointsPosition = new double[stringData.string.length];
         double[] pointsSpeed = new double[stringData.string.length];
         double[] pointsAcceleration = new double[stringData.string.length];
-        for (int i = 0; i < stringData.string.length-1; i++) {
+        for (int i = 1; i < stringData.string.length-1; i++) {
             pointsPosition[i] = Math.sin(stringData.string[i])/10000;
         }
         for (int i = 1; i < pointsAcceleration.length-1; i++) {
             pointsAcceleration[i] = (pointsPosition[i-1]-2*pointsPosition[i]+pointsPosition[i+1])/Math.pow(stringData.dx, 2);
         }
 
-        out.add(pointsToString("xi", stringData.string));
-        out.add(pointsToString("u", pointsPosition));
-        out.add(pointsToString("v", pointsSpeed));
-        out.add(pointsToString("a", pointsAcceleration));
+        out.add(pointsToString(t, "xi", stringData.string));
+        out.add(pointsToString(t, "u", pointsPosition));
+        out.add(pointsToString(t, "v", pointsSpeed));
+        out.add(pointsToString(t, "a", pointsAcceleration));
+
+        while (t < 2){
+            t += dt;
+
+            for (int i = 1; i < pointsPosition.length-1; i++) {
+                pointsPosition[i] += pointsSpeed[i]*dt;
+            }
+            for (int i = 1; i < pointsSpeed.length-1; i++) {
+                pointsSpeed[i] += pointsAcceleration[i]*dt;
+            }
+            for (int i = 1; i < pointsAcceleration.length-1; i++) {
+                pointsAcceleration[i] = (pointsPosition[i-1]-2*pointsPosition[i]+pointsPosition[i+1])/Math.pow(stringData.dx, 2);
+            }
+
+            out.add(pointsToString(t, "u", pointsPosition));
+            out.add(pointsToString(t, "v", pointsSpeed));
+            out.add(pointsToString(t, "a", pointsAcceleration));
+        }
 
         return out;
     }
 
-    public static String pointsToString(String type, double[] points){
+    public static String pointsToString(double t, String type, double[] points){
         StringBuilder out = new StringBuilder();
+        out.append("t").append(t).append(sep);
         out.append(type).append(sep);
         for (double point : points){
             out.append(String.valueOf(point).replaceAll("\\.", decimalSep)).append(sep);
